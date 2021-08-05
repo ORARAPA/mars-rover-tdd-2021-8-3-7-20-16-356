3d21c57b7a9909a395482a828e6cd134d92b57b7
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-05 19:03:41 +0800

    [MarsRover][Paraluman] feat: implement execute command L given locationX_0, locationY_0, direction_S

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -40,6 +40,9 @@ public class MarsRover {
         if(direction.equals("N")){
             newDirection = "W";
         }
+        if(direction.equals("S")){
+            newDirection = "E";
+        }
         roverStatus = new RoverStatus(locationX,locationY,newDirection);
     }
 
